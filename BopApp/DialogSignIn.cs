9ba890a2f752ffbbfbb3e3e33ff339a0a6da4a32
Dylan Tindale <Dylan Tindale>
2017-10-15 21:10:46 +1200

    Add project files.

---
+++ BopApp/DialogSignIn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BopApp
+{
+    class DialogSignIn : DialogFragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+            var view = inflater.Inflate(Resource.Layout.dialog_sign_in, container, false);
+            return view;
+        }
+        public override void OnActivityCreated(Bundle savedInstancesState)
+        {
+            Dialog.Window.RequestFeature(WindowFeatures.NoTitle);//removes title bar
+            base.OnActivityCreated(savedInstancesState);
+            Dialog.Window.Attributes.WindowAnimations = Resource.Style.dialog_animation;//animation set
+        }
+        
+    }
+}
---
+++ BopApp/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using System;
+
+namespace BopApp
+{
+    [Activity(Label = "BopApp", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private Button mBtnSignUp, mBtnSignIn;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            mBtnSignUp = FindViewById<Button>(Resource.Id.btnSignUp);
+            mBtnSignIn = FindViewById<Button>(Resource.Id.btnSignIn);
+            mBtnSignUp.Click += (object sender, EventArgs args) =>
+            {
+                //Pull up dialog
+                FragmentTransaction transaction = FragmentManager.BeginTransaction();
+                DialogSignUp dialogSignUp = new DialogSignUp();
+                dialogSignUp.Show(transaction, "dialog fragment");
+            };
+            mBtnSignIn.Click += (object sender, EventArgs args) =>
+            {
+                //Pull up dialog
+                FragmentTransaction transaction = FragmentManager.BeginTransaction();
+                DialogSignIn dialogSignIn = new DialogSignIn();
+                dialogSignIn.Show(transaction, "dialog fragment");
+            };
+        }
+    }
+}
+
